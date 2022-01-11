7f9b2c35ab818899ce8015e74e12e5097f2dced4
Eugene Krishtopa <eugene.krishtopa@requestum.com>
2022-01-11 16:37:15 +0200

    add husky and lint staged

--- src/components/Header/Index.tsx
+++ src/components/Header/Index.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Header() {
   return (
     <View style={styles.container}>
-      <Text>Header123</Text>
+      <Text> Header123</Text>
     </View>
   );
 }
